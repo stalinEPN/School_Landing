70ce8d45132dd52ca5328adc761c81057a129ca5
stalinEPN <rony.garcia01@epn.edu.ec>
2024-01-23 23:40:02 -0500

    cambios menores

--- src/app.py
+++ src/app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+
+    return render_template('views/home.html')
+
+
 def status_401(error):
     return redirect(url_for('login'))
 
@@ -13,4 +23,4 @@ if __name__ == '__main__':
     app.register_error_handler(401, status_401)
     app.register_error_handler(404, status_404)
 
-    app.run(port=5500)
+    app.run(port=5500, debug=True)
